623030b7bbe45453d1c9b6032f5dc8b9309cb406
SeanCushnie <133601523+SeanCushnie@users.noreply.github.com>
2023-05-19 15:02:55 +0100

    MVP met

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -47,6 +47,11 @@ def get_customer_cash(customer):
 def remove_customer_cash(customer, cash):
     customer["cash"] -= cash
 
+def get_customer_pet_count(customer):
+    return len(customer["pets"])
+
+def add_pet_to_customer(customer, new_pet):
+    customer["pets"].append(new_pet)
 
 
 
