28067c6e20863c4158bc592499874818a6c32284
SeanCushnie <133601523+SeanCushnie@users.noreply.github.com>
2023-05-19 12:43:06 +0100

    more completed :D

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -1,6 +1,33 @@
 # WRITE YOUR FUNCTIONS HERE
+import pdb
 def get_pet_shop_name(pet_shop):
     return pet_shop["name"]
-
+# pdb.set_trace()
 def get_total_cash(pet_shop):
     return pet_shop["admin"]["total_cash"]
+
+def add_or_remove_cash(pet_shop,cash):
+    # pdb.set_trace()
+    pet_shop["admin"]["total_cash"] += cash
+
+def get_pets_sold(pet_shop):
+    return pet_shop["admin"]["pets_sold"]
+
+def increase_pets_sold(pet_shop, sold):
+    pet_shop["admin"]["pets_sold"] += sold
+
+    
+
+
+
+
+
+
+
+def find_pet_by_name(pet_shop, pet_name):
+    # pdb.set_trace()
+    for pet in pet_shop["pets"]:
+        if pet["name"] == pet_name:
+            return pet
+        
+    
